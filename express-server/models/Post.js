85fccaa2a795d15c79001ea0b719a96021c129cc
Ayyub Jose <ajose3@depaul.edu>
2023-11-12 17:01:53 -0600

    addng the express-server

---
+++ express-server/models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+  title: { type: String, required: true },
+  todo: { type: String, required: false },
+  description: { type: String, required: false },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+});
+//Export model
+module.exports = mongoose.model("Post", PostSchema);
